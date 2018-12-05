f82bb2175b0a3c336bb32fb18b65098d7428da35
cnrd <hello@conradweise.com>
2018-12-05 19:37:28 +0100

    implemented arrow up functionality and style changes

--- knowbase.js
+++ knowbase.js
@@ -6,45 +6,66 @@ const submitButton = document.querySelector('#submit-btn')
 const input = document.querySelector('#input-text')
 
 let slides = []
-let currentSlide = 0
+let state = {
+  current_slide: 0,
+  mode: 'prep'
+}
 
 submitButton.addEventListener('click', () => {
   try {
     slides = JSON.parse(input.value)
+    present()
   } catch (err) {
-    window.alert('no valid input')
-    console.log(err)
-  }
+    input.className = 'invalid'
 
-  present()
+    setTimeout(() => {
+      input.className = ''
+    }, 400)
+  }
 })
 
 document.onkeydown = (event) => {
-  if (event.keyCode === 37) previous()
-  if (event.keyCode === 39) next()
+  if (state.mode === 'pres') {
+    if (event.keyCode === 37) previous()
+    if (event.keyCode === 39) next()
+    if (event.keyCode === 38) prepare()
+  }
+}
+
+function prepare () {
+  state.mode = 'prep'
+  state.current_slide = 0
+  
+  prepDiv.style.display = 'block'
+  presDiv.style.display = 'none'
 }
 
 function present () {
+  state.mode = 'pres'
+
   prepDiv.style.display = 'none'
-  presDiv.style.display = 'block'
+  presDiv.style.display = 'flex'
 
   changeSlide()
 }
 
 function changeSlide () {
-  console.log(currentSlide)
-  mainText.innerText = slides[currentSlide].text
-  mainImg.src = slides[currentSlide].img
+  mainText.innerText = slides[state.current_slide].text
+  mainImg.src = slides[state.current_slide].img
+
+  console.log(state.current_slide)
 }
 
 function previous () {
-  if (currentSlide === 0) return
-  currentSlide--
+  if (state.current_slide === 0) return
+
+  state.current_slide--
   changeSlide()
 }
 
 function next () {
-  if (currentSlide === slides.length - 1) return
-  currentSlide++
+  if (state.current_slide === slides.length - 1) return
+
+  state.current_slide++
   changeSlide()
 }
