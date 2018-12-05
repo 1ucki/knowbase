76ed2d21ed8c49cb5d70901349be5b737cddf572
1ucki <fwillen@gmail.com>
2018-12-05 16:10:57 +0100

    general update

--- knowbase.js
+++ knowbase.js
@@ -1,3 +1,5 @@
+const prepDiv = document.querySelector('#prep')
+const presDiv = document.querySelector('#pres')
 const mainText = document.querySelector('#main-text')
 const mainImg = document.querySelector('#main-pic')
 const submitButton = document.querySelector('#submit-btn')
@@ -6,18 +8,27 @@ const input = document.querySelector('#input-text')
 let slides = []
 let currentSlide = 0
 
-
 submitButton.addEventListener('click', () => {
   try {
     slides = JSON.parse(input.value)
   } catch (err) {
+    window.alert('no valid input')
     console.log(err)
   }
+
+  present()
 })
 
 document.onkeydown = (event) => {
   if (event.keyCode === 37) previous()
   if (event.keyCode === 39) next()
+}
+
+function present () {
+  prepDiv.style.display = 'none'
+  presDiv.style.display = 'block'
+
+  changeSlide()
 }
 
 function changeSlide () {
